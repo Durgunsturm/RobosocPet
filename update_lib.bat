df95f696dc3df8c63e8220949774795b9789c179
gean <88561178+geanyop@users.noreply.github.com>
2024-12-06 17:41:56 +0000

    improved library system

--- update_lib.bat
+++ update_lib.bat
@@ -1,2 +1,16 @@
-robocopy .\lib C:\Users\edenr\Documents\Arduino\libraries\petlib
+@echo off
+mkdir "lib-pack"
+echo. > "lib-pack\lib.h"
+echo. > "lib-pack\lib.c"
+for /r lib\ %%? in ("*.h") do (
+        echo %%~sn?
+        echo. >> "lib-pack\lib.h"
+        type %%~f? >> "lib-pack\lib.h"
+)
+for /r lib\ %%? in ("*.c") do (
+        echo %%~sn?
+        echo. >> "lib-pack\lib.c"
+        type %%~f? >> "lib-pack\lib.c"
+)
+rem robocopy .\lib-pack %UserProfile%\Documents\Arduino\libraries\petlib
 pause
